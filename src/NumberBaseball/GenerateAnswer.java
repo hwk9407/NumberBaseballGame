899b9e5e02a69aabaf7cdc3f42b7b8682ba539e9
hwk9407 <hwk__@naver.com>
2024-09-12 17:15:08 +0900

    feat(GenerateAnswer): 랜덤한 세 자리 수 생성하는 클래스 추가

--- src/NumberBaseball/GenerateAnswer.java
+++ src/NumberBaseball/GenerateAnswer.java
@@ -1,5 +1,9 @@
 package NumberBaseball;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GenerateAnswer {
     /**
      * NumberBaseball/GenerateAnswer.java
@@ -7,4 +11,20 @@ public class GenerateAnswer {
      * 1 ~ 9의 숫자만 사용해야 하며(0은 안된다.)
      * 각 자리수의 수들은 중복되지 않아야 한다. (123, 234는 되지만 333, 112는 안됨)
      */
+    private List<Integer> randList;
+
+    // 생성자
+    public GenerateAnswer() {
+        this.randList = new ArrayList<>(); // 순서가 중요하나 중복을 허용하면 안됨.
+        for (int i = 1; i <= 9; i++) {  // randList : [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            this.randList.add(i);
+        }
+        Collections.shuffle(this.randList);  // 섞는 메서드
+        this.randList = this.randList.subList(0, 3); // 0, 1, 2 3개의 요소만 가져옴
+    }
+
+    // getter
+    public List<Integer> getAnswer() {
+        return this.randList;
+    }
 }
