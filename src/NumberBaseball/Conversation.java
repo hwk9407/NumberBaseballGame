ca762d02b150bf988df1ae53144f045db7d02003
hwk9407 <hwk__@naver.com>
2024-09-13 19:12:44 +0900

    feat(Conversation): 정답을 입력받고, 맞출시 축하메시지를 주는 메서드 추가

--- src/NumberBaseball/Conversation.java
+++ src/NumberBaseball/Conversation.java
@@ -1,5 +1,9 @@
 package NumberBaseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Conversation {
     /**
      * NumberBaseball/Conversation.java
@@ -7,4 +11,22 @@ public class Conversation {
      * 정답을 입력할 때까지 계속해서 진행하도록 반복문 사용
      * 정답을 맞출시 축하메시지와 함께 종료
      */
+
+
+    public void startInputNumber() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("서로 다른 세 자리의 숫자를 입력해주세요.");
+        while (true) {
+            String inputStr = sc.nextLine();
+            AnswerCheck answerCheck = new AnswerCheck();
+
+            if (answerCheck.scoreCalculate(inputStr)) break;
+        }
+        sc.close(); // Scanner 닫기
+    }
+
+    public void congratulationMessage() {
+        System.out.println("축하합니다! 정답을 맞추셨습니다.");
+    }
 }
