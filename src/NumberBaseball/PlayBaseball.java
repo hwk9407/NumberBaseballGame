88db083f2413918ddb111b87d99d6d7ec34a6832
hwk9407 <hwk__@naver.com>
2024-09-18 00:53:11 +0900

    fix(PlayBaseball): 미리 정답 출력되지 않게 수정

--- src/NumberBaseball/PlayBaseball.java
+++ src/NumberBaseball/PlayBaseball.java
@@ -9,9 +9,7 @@ public class PlayBaseball {
      * 생성자에 정답생성 객체를 호출
      * 실행 메서드를 통해 게임 진행
      */
-
-    private GenerateAnswer correctAnswer;
-
+    
     public PlayBaseball() {
         System.out.println("< 숫자 야구 게임 > 을 시작하겠습니다.");
     }
@@ -19,15 +17,15 @@ public class PlayBaseball {
     public void execute() {
         Conversation conversation = new Conversation();
 
-        this.correctAnswer = new GenerateAnswer();
-
-        System.out.println("-----테스트 전용 정답공개 나중에 지워야함~!-----");
+        GenerateAnswer correctAnswer = new GenerateAnswer();
+/*
+        // 정답 테스트용
         System.out.println("정답은 : " + correctAnswer.getAnswer());
-        System.out.println("-----테스트 전용 정답공개 나중에 지워야함~!-----");
+*/
 
 
         while (true) {
-            AnswerCheck answerCheck = new AnswerCheck(this.correctAnswer);
+            AnswerCheck answerCheck = new AnswerCheck(correctAnswer);
 
             // 사용자 입력받기 실행
             List<Integer> userAnswer = conversation.inputNumber();
