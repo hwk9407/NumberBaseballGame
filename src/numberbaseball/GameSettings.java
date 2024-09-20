5cccc182146b69580997f595c825b307446031df
hwk9407 <hwk__@naver.com>
2024-09-20 17:36:20 +0900

    refactor: 통계 클래스와 현재게임세팅 클래스의 역할을 분리

--- src/numberbaseball/GameSettings.java
+++ src/numberbaseball/GameSettings.java
@@ -5,26 +5,18 @@ import java.util.ArrayList;
 public class GameSettings {
     /**
      * numberbaseball/GameSettings.java
-     * 게임 난이도와, 정답 시도 횟수 필드를 저장하고 관리하는 클래스
+     * 진행할 게임 난이도를 저장하고 관리하는 클래스
      */
-    private GameStatistics gameStatistics;
-    private int gameDifficulty;
+    private int difficulty;
 
     public GameSettings () {
-        this.gameDifficulty = 3;
-        this.gameStatistics = new GameStatistics();
+        this.difficulty = 3;
     }
 
-    public int getGameDifficulty() {
-        return gameDifficulty;
+    public int getDifficulty() {
+        return difficulty;
     }
-    public void setGameDifficulty(int gameDifficulty) {
-        this.gameDifficulty = gameDifficulty;
-    }
-    public void addTryCount(int count) {
-        gameStatistics.addTryCount(count);
-    }
-    public ArrayList<Integer> getTryCount() {
-        return gameStatistics.getTryCount();
+    public void setDifficulty(int gameDifficulty) {
+        this.difficulty = gameDifficulty;
     }
 }
--- src/numberbaseball/GameStatistics.java
+++ src/numberbaseball/GameStatistics.java
@@ -5,18 +5,25 @@ import java.util.ArrayList;
 public class GameStatistics {
     /**
      * numberbaseball/GameStatistics.java
-     * 게임 시도 횟수 기록을 관리하는 클래스
+     * 게임 시도별 횟수, 난이도의 통계를 기록하고 관리하는 클래스
      */
-    private ArrayList<Integer> tryCount;
+    private final ArrayList<Integer> tryCount;
+    private final ArrayList<Integer> tryDifficulty;
+
 
     GameStatistics() {
         tryCount = new ArrayList<>();
+        tryDifficulty = new ArrayList<>();
     }
 
     public ArrayList<Integer> getTryCount() {
         return tryCount;
     }
-    public void addTryCount(int count) {
+    public ArrayList<Integer> getTryDifficulty() {
+        return tryDifficulty;
+    }
+    public void addTry(int count, int difficulty) {
         tryCount.add(count);
+        tryDifficulty.add(difficulty);
     }
 }
