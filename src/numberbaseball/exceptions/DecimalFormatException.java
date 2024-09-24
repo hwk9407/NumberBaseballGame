8341f1e5c5e85cc91b0846b58cd054372456b4e7
hwk9407 <hwk__@naver.com>
2024-09-24 10:44:59 +0900

    docs: 예외 클래스들의 주석 내용 추가 및 수정

--- src/numberbaseball/exceptions/DecimalFormatException.java
+++ src/numberbaseball/exceptions/DecimalFormatException.java
@@ -1,6 +1,11 @@
 package numberbaseball.exceptions;
 
 public class DecimalFormatException extends Exception {
+    /**
+     * numberbaseball.exceptions.DoublerInputException.java
+     * 숫자 입력은 하였으나, 소수점을 두개이상 혹은 ".12" "123."과 같은
+     * 이상한 형태의 실수를 입력시 발생하는 예외
+     */
     public DecimalFormatException() {
         super("이상한 형태의 소수점입니다.");
     }
--- src/numberbaseball/exceptions/DoubleInputException.java
+++ src/numberbaseball/exceptions/DoubleInputException.java
@@ -2,7 +2,7 @@ package numberbaseball.exceptions;
 
 public class DoubleInputException extends Exception {
     /**
-     * numberbaseball/exceptions/DoublerInputException.java
+     * numberbaseball.exceptions.DoublerInputException.java
      * 숫자 입력은 하였으나, 실수를 입력할 경우 발생하는 예외
      */
     public DoubleInputException() {
